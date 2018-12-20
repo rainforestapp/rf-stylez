8668c09e7821a08627d47a5d7cccb437918f4812
shane <shane@rainforestapp.com>
2018-12-19 19:04:09 -0600

    Create cop for suggesting people use API::Base and not Grape::API

--- lib/rf/stylez/version.rb
+++ lib/rf/stylez/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 module Rf
   module Stylez
-    VERSION = '0.2.13'
+    VERSION = '0.2.14'
   end
 end
--- spec/rubocop/cop/lint/no_http_party_spec.rb
+++ spec/rubocop/cop/lint/no_http_party_spec.rb
@@ -4,14 +4,14 @@ describe RuboCop::Cop::Lint::NoHTTParty do
   let(:config) { RuboCop::Config.new }
   subject(:cop) { described_class.new(config) }
 
-  it 'registers an offense when using `#ENV`' do
+  it 'registers an offense when using `HTTParty`' do
     expect_offense(<<-RUBY.strip_indent)
       HTTParty.get('foo')
       ^^^^^^^^^^^^^^^^^^^ Prefer `TimedRequest` instead of raw `HTTParty` calls.
     RUBY
   end
 
-  it 'does not register an offense when using `#TimedRequest`' do
+  it 'does not register an offense when using `TimedRequest`' do
     expect_no_offenses(<<-RUBY.strip_indent)
       TimedRequest.get('foo')
     RUBY
