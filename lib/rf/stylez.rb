413e9455e24ecd5c88eb25af1de5f5208c7eb838
Bartek Kruszczynski <bartek@rainforestapp.com>
2019-07-16 18:20:31 +0200

    RF-9850 Add a self-check for latest version

--- lib/rf/stylez.rb
+++ lib/rf/stylez.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require 'json'
+require 'net/http'
+require 'logger'
 require 'rf/stylez/version'
 require 'rubocop/cop/lint/no_env'
 require 'rubocop/cop/lint/no_json'
@@ -11,6 +14,25 @@ require 'rubocop/cop/lint/no_untyped_raise'
 
 module Rf
   module Stylez
-    # Your code goes here...
+    RUBYGEMS_URL = URI('https://rubygems.org/api/v1/gems/rf-stylez.json').freeze
+
+    def self.check_for_updates
+      remote_version = JSON.parse(Net::HTTP.get(RUBYGEMS_URL))['version']
+      if remote_version == VERSION
+        STDOUT.puts
+        STDOUT.puts('You are running latest rf-stylez ')
+        STDOUT.puts('(•_•) ( •_•)>⌐■-■ (⌐■_■)')
+        STDOUT.puts
+      else
+        STDERR.puts
+        STDERR.puts('Please update rf-stylez: gem update rf-stylez')
+        STDERR.puts("#{remote_version} is newest and you are running #{VERSION}")
+        STDERR.puts
+      end
+    rescue SocketError
+      STDOUT.puts('Offline, cannot check for rf-stylez updates')
+    end
+
+    check_for_updates
   end
 end
