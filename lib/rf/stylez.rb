9a10780732efa44ce0e3ea4e2883ecbb2af8035a
Paul Padier <paul.padier@outlook.com>
2020-05-20 16:51:48 +0900

    Remove Lint/NoENV definition, require get_env instead

--- lib/rf/stylez.rb
+++ lib/rf/stylez.rb
@@ -3,7 +3,6 @@
 require 'rf/stylez/version'
 require 'rf/stylez/update_check'
 require 'rubocop'
-require 'rubocop/cop/lint/no_env'
 require 'rubocop/cop/lint/no_json'
 require 'rubocop/cop/lint/no_http_party'
 require 'rubocop/cop/lint/obscure'
