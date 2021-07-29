5835501f10ab09529a5125292d5a10086036f04f
Paul Padier <paul.padier@outlook.com>
2021-07-29 17:26:28 +0900

    Add NoVCRRecording cop

---
+++ lib/rubocop/cop/lint/no_vcr_recording.rb
@@ -0,0 +1,74 @@
+# frozen_string_literal: true
+require 'unparser'
+
+module RuboCop
+  module Cop
+    module Lint
+      # @example
+      #   # bad
+      #   vcr: { record_mode: :once, tag: :foo }
+      #   vcr: { record_mode: :none }
+      #
+      #   # good
+      #   vcr: { tag: :foo }
+      #   vcr: true
+      class NoVCRRecording < RuboCop::Cop::Base
+        extend AutoCorrector
+
+        MSG = 'Do not set :record option in VCR'.freeze
+
+        def_node_search :is_only_setting_record_option?, <<~PATTERN
+          (pair
+            (sym :vcr)
+            (hash
+              (pair
+                (sym :record)
+                ...
+              )
+            )
+          )
+        PATTERN
+        def_node_search :is_setting_record_option?, <<~PATTERN
+          (pair
+            (sym :vcr)
+            (hash
+              <
+                (pair
+                  (sym :record)
+                  ...
+                )
+                ...
+              >
+            )
+          )
+        PATTERN
+
+        def on_pair(node)
+          return unless is_only_setting_record_option?(node) || is_setting_record_option?(node)
+
+          add_offense(node) do |corrector|
+            if is_only_setting_record_option?(node)
+              corrector.replace(node, 'vcr: true')
+            elsif is_setting_record_option?(node)
+              corrector.replace(node, remove_record_option(node))
+            end
+          end
+        end
+
+        private
+
+        def remove_record_option(top_pair_node)
+          extend AST::Sexp
+
+          _vcr_key, hash = top_pair_node.children
+          other_options = hash.children.reject do |pair|
+            key, _value = pair.children
+            key == s(:sym, :record)
+          end
+
+          Unparser.unparse(s(:pair, s(:sym, :vcr), s(:hash, *other_options)))
+        end
+      end
+    end
+  end
+end
---
+++ spec/rubocop/cop/lint/no_vcr_recording_spec.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+describe RuboCop::Cop::Lint::NoVCRRecording do
+  let(:config) { RuboCop::Config.new }
+  subject(:cop) { described_class.new(config) }
+
+  it 'registers an offense when using vcr: { record: ... }' do
+    expect_offense(<<~RUBY)
+      describe Foo, vcr: { record: :new_episodes } do
+                    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Do not set :record option in VCR
+        include_examples 'all the networks!'
+      end
+    RUBY
+
+    expect_correction(<<~RUBY)
+      describe Foo, vcr: true do
+        include_examples 'all the networks!'
+      end
+    RUBY
+  end
+
+  it 'handles multiple options passed to vcr' do
+    expect_offense(<<~RUBY)
+      it 'works!', vcr: { tag: :workworkwork, record: :once } do
+                   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Do not set :record option in VCR
+        expect(let_me_google_that_for_you).to be_full_of_sass
+      end
+    RUBY
+
+    expect_correction(<<~RUBY)
+      it 'works!', vcr: { tag: :workworkwork } do
+        expect(let_me_google_that_for_you).to be_full_of_sass
+      end
+    RUBY
+  end
+
+  it 'does not register an offense when not using :record' do
+    expect_no_offenses(<<~RUBY)
+      context 'with no recording', vcr: true do
+        specify { expect(true).to eq(true) }
+      end
+    RUBY
+
+    expect_no_offenses(<<~RUBY)
+      context 'with no recording', vcr: { match_requests_on: [:request, :path] } do
+        specify { expect(false).to eq(false) }
+      end
+    RUBY
+  end
+end
